99caa3a8005efa9e2a1efb189673a5070a4bc9a0
Manju KC <Manju KC>
2019-06-05 19:03:01 -0400

    get is working

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const knex = require("knex");
 const helmet = require('helmet');
 const server = express();
+// const cohortRouter = require('./router/cohort.js')
+const knexConfig = require('./knexfile.js')
+const db = knex(knexConfig.development);
+
+
 server.use(helmet());
 server.use(express.json());
+// server .use('/api/cohorts',  cohortRouter)
+
+
+server.get("/api/cohorts", (req, res) => {
+    console.log('i am here')
+    db("cohorts")
+      .then(cohort => {
+        res.status(200).json(cohort);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
