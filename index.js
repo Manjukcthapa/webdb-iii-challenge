b5f02e21a453765fda00bbf24f218f5b66719582
Manju KC <Manju KC>
2019-06-05 19:09:54 -0400

    post and getbyid is working

--- index.js
+++ index.js
@@ -23,6 +23,48 @@ server.get("/api/cohorts", (req, res) => {
       });
   });
 
+
+  server.post("/api/cohorts", (req, res) => {
+    if (!req.body.name) {
+      res.status(400).json({ msg: "please provide a name" });
+    } else {
+      db("cohorts")
+        .insert(req.body, "id")
+        .then(ids => {
+          db("cohorts")
+            .where({ id: ids[0] })
+            .first()
+            .then(cohort => {
+              res.status(200).json(cohort);
+            })
+            .catch(err => {
+              res.status(500).json(err);
+            });
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
+    }
+  });
+
+server.get("/api/cohorts/:id", (req, res) => {
+    db("cohorts")
+      .where({ id: req.params.id })
+      .first()
+      .then(cohort => {
+        if (cohort) {
+          res.status(200).json(cohort);
+        } else {
+          res.status(404).json({ message: "no such cohorts started yet" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
  console.log(`\n** API running on http://localhost:${port} **\n`)
